13a315b4c87d4dfedb421ec29664b3bee8b8bc8a
Dani Vijay <danimvijay@gmail.com>
2018-03-13 22:40:52 +0530

    add inquire, github

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const figlet = require('figlet')
 
 const files = require('./lib/files')
 
-clear();
+clear()
 console.log(
   chalk.yellow(
     figlet.textSync('github-cli', { horizontalLayout: 'full' })
   )
 )
+
+const inquirer  = require('./lib/inquire')
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials()
+  console.log(credentials)
+}
+
+run()
